e44b60fc3a71ee86d7cb4353a051815ac1bf4267
Martonos Magor <martonosmagor@yahoo.com>
2021-08-02 11:15:29 +0300

    Creating the fetch.html

--- hello.py
+++ hello.py
@@ -3,12 +3,16 @@ import json
 app = Flask(__name__)
 app.secret_key = "hello"
 
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 @app.route("/", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
         user = request.form["nm"]
-        animal = request.form["animal"]
-        return f"<h1>{animal}</h1>"
+
+
 
         session["user"] = user
         if not user:
@@ -17,15 +21,22 @@ def login():
     else:
         return render_template("home.html")
 
-@app.route("/fetch_data")
+@app.route("/fetch_data",  methods=["POST", "GET"])
 def user():
     if "user" in session:
         user = session["user"]
-        return f"<h1>{user}</h1>"
+        animal = "foxes"
+
+
+        with open('pictures.json') as f:
+            data = json.load(f)
+
+            for animal in data['dogs']:
+                name = (animal)['name']
+                image = (animal['image'])
+
+        return render_template("fetch.html", names = name, image = image)
     else:
         return redirect(url_for("home.html"))
 
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
